77b52805a56c243c253a16902e3c1f410c4a2881
Juskeerat Anand <juskeerat@gmail.com>
2023-04-12 16:55:43 -0700

    Update question5.py

--- ArineChallenge/question5.py
+++ ArineChallenge/question5.py
@@ -3,18 +3,17 @@ from typing import Optional
 from datetime import datetime
 
 def get_latest_med_ndc(data_obj: dict) -> Optional[str]:
-    latest_fill_date = None
-    latest_ndc9 = None
+    latest_date = None
+    ndc9_latest = None
     
     if "medications" in data_obj:
-        for med in data_obj["medications"]:
-            if "fills" in med:
-                for fill in med["fills"]:
+        for i in data_obj["medications"]:
+            if "fills" in i:
+                for fill in i["fills"]:
                     if "fillDate" in fill:
                         fill_date = datetime.strptime(fill["fillDate"], "%Y-%m-%d")
-                        if latest_fill_date is None or fill_date > latest_fill_date:
-                            latest_fill_date = fill_date
-                            latest_ndc9 = med["ndc9"]
+                        if latest_date is None or fill_date > latest_date:
+                            latest_date = fill_date
+                            ndc9_latest = i["ndc9"]
                             
-    return latest_ndc9
-
+    return ndc9_latest
