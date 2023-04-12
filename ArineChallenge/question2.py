b7dd6dd402e2066a22cdecf728fa1fbb367665cf
Juskeerat Anand <juskeerat@gmail.com>
2023-04-12 16:59:57 -0700

    Update question2.py

--- ArineChallenge/question2.py
+++ ArineChallenge/question2.py
@@ -1,8 +1,4 @@
-# def convert_to_float(input_str: str, default: float): 
-#     if not input_str.replace(".", "").isnumeric():
-#         return default
-#     else: 
-#         return float(input_str)
+
 
 def convert_to_float(input_str: str, default: float) -> float:
     if '.' not in input_str:
